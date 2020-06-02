2ef3bf7d0a410f44a86c4309dd2b40bc60e186e5
İlker Çakır <ilker.cakir@betbull.com>
2020-06-03 01:32:38 +0300

    audit adjustments

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,10 +7,10 @@ const Footer = () => {
   return (
     <footer className="flex justify-center flex-shrink-0 w-full border-t py-2 bg-white">
       <div className="flex flex-col">
-        <a href="https://github.com/cakirilker/blog" target="_blank" className="flex justify-center mb-2 text-gray-500 hover:text-black transition duration-500 ease-in-out">
+        <a href="https://github.com/cakirilker/blog" target="_blank" rel="noreferrer" aria-label="View source on Github" className="flex justify-center mb-2 text-gray-900 hover:text-black transition duration-500 ease-in-out">
           <FontAwesomeIcon icon={faGithub} className="mx-auto fill-current" size="2x" />
         </a>
-        <p className="text-sm text-gray-500 m-0">made with<FontAwesomeIcon icon={faHeart} className="mx-1 fill-current text-red-600" />by Ilker Cakir</p>
+        <p className="text-sm text-gray-900 m-0">made with<FontAwesomeIcon icon={faHeart} className="mx-1 fill-current text-red-600" />by Ilker Cakir</p>
       </div>
     </footer>
   );
